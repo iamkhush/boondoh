a237561f033cc308a285fd9e787ae21d55d1b579
Ankush Chadda <ankush.chadda@gmail.com>
2011-09-22 11:32:56 +0530

    made changes in the script

--- judges_into_db.py
+++ judges_into_db.py
@@ -5,11 +5,10 @@ sh = xlrd.open_workbook(sys.argv[1])
 sheet = sh.sheet_by_index(0)
 db = MySQLdb.connect(user="root",passwd="12345",db="ITC")
 c = db.cursor()
+rownum = 1
+print   """ INSERT INTO `ITC`.`IP_cases_district_courts_and_judges` (`id`, `judge_first_name`, `judge_middle_name`, `judge_last_name`, `birth_year`, `court_name`, `president_name`, `part_affliation_of_president`, `ABA_rating`, `senate_vote_date`, `commission_date`, `retirement`, `termination_date`) VALUES (%d,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """ %(rownum,sheet.row_values(rownum)[0],sheet.row_values(rownum)[1],sheet.row_values(rownum)[2],sheet.row_values(rownum)[3],sheet.row_values(rownum)[4],sheet.row_values(rownum)[5],sheet.row_values(rownum)[6],sheet.row_values(rownum)[7],sheet.row_values(rownum)[8],sheet.row_values(rownum)[9],sheet.row_values(rownum)[10],sheet.row_values(rownum)[11])
 for rownum in range(1,sheet.nrows):
-    c.execute(""" INSERT INTO IP_cases_district_courts_and_judges VALUES
-               (%s %s %s %s %s %s %s %s %s %s %s %s) """ %(row_values(rownum)[0],row_values(rownum)[1],row_values(rownum)[2],row_values(rownum)[3],
-                                                           row_values(rownum)[4],row_values(rownum)[5],row_values(rownum)[6],
-                                                           row_values(rownum)[7],row_values(rownum)[8],row_values(rownum)[9],
-                                                           row_values(rownum)[10],row_values(rownum)[11]))
+    c.execute(""" INSERT INTO `ITC`.`IP_cases_district_courts_and_judges` (`id`, `judge_first_name`, `judge_middle_name`, `judge_last_name`, `birth_year`, `court_name`, `president_name`, `part_affliation_of_president`, `ABA_rating`, `senate_vote_date`, `commission_date`, `retirement`, `termination_date`) VALUES 
+               (%d,'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') """ %(rownum,sheet.row_values(rownum)[0],sheet.row_values(rownum)[1],sheet.row_values(rownum)[2],sheet.row_values(rownum)[3],sheet.row_values(rownum)[4],sheet.row_values(rownum)[5],sheet.row_values(rownum)[6],sheet.row_values(rownum)[7],sheet.row_values(rownum)[8],sheet.row_values(rownum)[9],sheet.row_values(rownum)[10],sheet.row_values(rownum)[11]))
 
     
